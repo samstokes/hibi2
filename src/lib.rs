a91943f84c5c0df754394c7360d2e7e103b1e94f
Sam Stokes <me@samstokes.co.uk>
2018-10-26 14:49:25 -0500

    only use dotenv in debug builds

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,12 @@ pub mod time;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+#[cfg(debug_assertions)]
 use dotenv::dotenv;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
+    #[cfg(debug_assertions)]
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
